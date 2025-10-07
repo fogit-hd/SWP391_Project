324975068c811f8ffbb203dde4f72f7551979632
Phong Huynh <huynhdungphong2005@gmail.com>
2025-10-08 01:37:00 +0700

    Implement routing and state management in App component

--- FE/SWP391_FE/src/App.jsx
+++ FE/SWP391_FE/src/App.jsx
@@ -1,10 +1,53 @@
-import { Button } from "antd";
-import { ButtonGroup } from "react-bootstrap";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProtectedRoute from "components/protected-route";
+import ManageAccount from "./pages/admin/manageAccount";
+import { ToastContainer } from "react-toastify";
+import Homepage from "/src/pages/home";
+import LoginPage from "/src/pages/login";
+import RegisterPage from "/src/pages/register";
+import Dashboard from "/src/pages/admin/dashboard";
+import TermsPage from "/src/pages/terms";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/dashboard",
+      element: (
+        // <ProtectedRoute role={"ADMIN"}>
+        <Dashboard />
+        // </ProtectedRoute>
+      ),
+    },
+    {
+      path: "/manage-account",
+      element: (
+        // <ProtectedRoute role={"ADMIN"}>
+        <ManageAccount />
+        // </ProtectedRoute>
+      ),
+    },
+    {
+      path: "/",
+      element: <Homepage />,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+    {
+      path: "/terms",
+      element: <TermsPage />,
+    },
+  ]);
+
   return (
     <>
-      <div>Frontend SWP391 Project</div>
+      <ToastContainer />
+      <RouterProvider router={router} />
     </>
   );
 }
